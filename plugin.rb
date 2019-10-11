ca205f63e8b8852154fddd30bf09c994070727e2
Leo McArdle <lmcardle@mozilla.com>
2019-10-11 18:59:38 +0100

    FIX: update all categories on startup

--- plugin.rb
+++ plugin.rb
@@ -1,10 +1,16 @@
 # name: auto-email-in
 # about: Discourse plugin which automatically sets category email-in addresses based on their slug
-# version: 1.2.0
+# version: 1.2.1
 # authors: Leo McArdle
 # url: https://github.com/mozilla/discourse-auto-email-in
 
 enabled_site_setting :auto_email_in_enabled
+
+def update_all_categories
+  ::Category.all.each do |category|
+    category.update_email_in
+  end
+end
 
 after_initialize do
   require_dependency 'category'
@@ -46,12 +52,14 @@ after_initialize do
       end
     end
   end
+
+  if SiteSetting.auto_email_in_enabled
+    update_all_categories
+  end
 end
 
 DiscourseEvent.on(:site_setting_changed) do |name|
   if name == :auto_email_in_enabled or name == :auto_email_in_divider or name == :auto_email_in_domain
-    Category.all.each do |category|
-      category.update_email_in
-    end
+    update_all_categories
   end
 end
